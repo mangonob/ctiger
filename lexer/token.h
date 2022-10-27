763803bd797c204a44cb12ccb24c14f58ba9b015
mangonob <mangonob@qq.com>
2022-10-27 15:52:58 +0800

    feat: text sort

---
+++ lexer/token.h
@@ -0,0 +1,59 @@
+#ifndef __TOKEN_HEADER__
+
+typedef union
+{
+  int pos;
+  int ival;
+  char *sval;
+} YYSTYPE;
+
+extern YYSTYPE yylval;
+
+typedef enum
+{
+  ID = 257,
+  STRING,
+  INT,
+  COMMA,
+  COLON,
+  SEMICOLON,
+  LPAREN,
+  RPAREN,
+  LBRACK,
+  RBRACK,
+  LBRACE,
+  RBRACE,
+  DOT,
+  PLUS,
+  MINUS,
+  TIMES,
+  DIVIDE,
+  EQ,
+  NEQ,
+  LT,
+  LE,
+  GT,
+  GE,
+  AND,
+  OR,
+  ASSIGN,
+  ARRAY,
+  IF,
+  THEN,
+  ELSE,
+  WHILE,
+  FOR,
+  TO,
+  DO,
+  LET,
+  IN,
+  END,
+  OF,
+  BREAK,
+  NIL,
+  FUNCTION,
+  VAR,
+  TYPE,
+} Token;
+
+#endif
--- main.c
+++ main.c
@@ -1,7 +1,33 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#define BUFFER_SIZE 1024
+#define COUNT 4
+
+int stringCompar(const void *lhs, const void *rhs);
 
 int main(int argc, char *argv[])
 {
-  printf("Hello world!");
+  char text[COUNT][BUFFER_SIZE] = {};
+  char *ptr[COUNT];
+
+  for (int i = 0; i < COUNT; i++)
+  {
+    scanf("%s", text[i]);
+    ptr[i] = text[i];
+  }
+
+  qsort(ptr, COUNT, sizeof(char *), stringCompar);
+
+  for (int i = 0; i < COUNT; i++)
+    puts(ptr[i]);
+
   return 0;
+}
+
+int stringCompar(const void *lhs, const void *rhs)
+{
+  char *l = *(char **)lhs;
+  char *r = *(char **)rhs;
+  return strcmp(l, r);
 }
