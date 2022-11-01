d427c88499187b0132bd71dded1381567dc20e82
mangonob <mangonob@qq.com>
2022-11-01 22:41:30 +0800

    feat: count file char

--- main.c
+++ main.c
@@ -1,18 +1,34 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <stdatomic.h>
 #include "foo.h"
 
 int main(int argc, char *argv[])
 {
-  int n;
+  if (argc != 2)
+  {
+    printf("Usage: %s filename\n", argv[0]);
+    exit(EXIT_FAILURE);
+  }
 
-  printf("Input random number count: ");
-  scanf("%d", &n);
-  srand(time(0));
+  FILE *f;
+  if ((f = fopen(argv[1], "r")) == NULL)
+  {
+    printf("Error: cannot open file %s", argv[1]);
+    exit(EXIT_FAILURE);
+  }
 
-  for (int i = 0; i < n; i++)
-    printf("%lf\n", randf());
+  char ch;
+  int count = 0;
+  while ((ch = getc(f)) != EOF)
+  {
+    putc(ch, stdout);
+    count++;
+  }
+  fclose(f);
+
+  printf("\nTotal char count is %d\n", count);
 
   return 0;
 }
