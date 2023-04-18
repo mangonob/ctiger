88459e91b047ba2429ee221273b91df3971f2d39
mangonob <mangonob@qq.com>
2023-04-18 16:24:16 +0800

    WIP: intermediate representation

---
+++ include/translate.h
@@ -0,0 +1,45 @@
+#ifndef TRANSLATE_H_
+#define TRANSLATE_H_
+
+#include "tree.h"
+
+typedef struct patchList_ *patchList;
+typedef struct Tr_exp_ *Tr_exp;
+
+struct Cx
+{
+  patchList trues;
+  patchList falses;
+  T_stm stm;
+};
+
+struct Tr_exp_
+{
+  enum
+  {
+    Tr_ex,
+    Tr_nx,
+    Tr_cx
+  } kind;
+
+  union
+  {
+    T_exp ex;
+    T_stm nx;
+    struct Cx cx;
+  };
+};
+
+struct patchList_
+{
+  Temp_label *head;
+  patchList tail;
+};
+
+static patchList PatchList(Temp_label head, patchList tail);
+
+static Tr_exp Tr_Ex(T_exp ex);
+static Tr_exp Tr_Nx(T_stm nx);
+static Tr_exp Tr_Cx(patchList trues, patchList falses, T_stm stm);
+
+#endif
